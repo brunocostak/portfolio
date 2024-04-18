8a2646fc73fa53be0252a9b510cc251c2d6441c6
bruno <bruno.costa1203@yahoo.com>
2024-04-18 12:56:45 -0300

    Atualização

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import Header from './components/Header'
 import { FaLinkedin, FaGithub, FaEnvelope } from 'react-icons/fa';
+import { FaRegNewspaper } from "react-icons/fa6";
 import 'animate.css';
 import './App.css'
 import Projects from './components/Projects';
 import Skills from './components/Skills';
+import { Formacao } from './components/Formacao';
 
 function App() {
 
   return (
     <>
       <Header />
-      <section className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pb-10 pt-20 flex-col items-center justify-center">
+      <section className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pb-10 md:pt-60 pt-32 flex-col items-center justify-center">
         <div>
           <h1 className="text-4xl font-bold text-center animate__animated animate__fadeIn">Olá, meu nome é Bruno</h1>
           <p className="mt-4 text-center animate__animated animate__fadeIn"> Sou um desenvolvedor versátil com experiência em
@@ -33,8 +35,14 @@ function App() {
                     <FaEnvelope className='animate__animated animate__pulse animate__infinite' size={48} />
                 </a>
             </li>
+            <li>
+                <a title="curriculo" href="https://drive.google.com/file/d/1kQu_noZ9bi2Xv_L0Ed-6r42aB0VI0Or7/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+                    <FaRegNewspaper className='animate__animated animate__pulse animate__infinite' size={48} />
+                </a>
+            </li>
         </ul>
       </section>
+      <Formacao />
       <Skills />
       <Projects />
     </>
---
+++ src/components/Formacao.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export const Formacao = () => {
+
+    return (
+        <section className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pb-10 pt-20 flex-col items-center justify-center">
+            <h2 className="text-4xl font-bold text-center animate__animated animate__fadeIn pb-10">Formação</h2>
+            <div className="py-10">
+                <h3 className="text-2xl font-bold text-center animate__animated animate__fadeIn">Análise e Desenvolvimento de Sistemas - UNINTER</h3>
+                <p className="mt-4 text-center animate__animated animate__fadeIn">2022 - Cursando, o curso de Análise e Desenvolvimento de Sistemas da UNINTER tem duração de 3 anos e é focado em desenvolvimento de software,
+                 banco de dados, análise de sistemas e programação.</p>
+            </div>
+            <div className="py-10">
+                <h3 className="text-2xl font-bold text-center animate__animated animate__fadeIn">Desenvolvimento Web Fullstack - Trybe</h3>
+                <p className="mt-4 text-center animate__animated animate__fadeIn">2022 - 2023, o curso de Desenvolvimento Web Fullstack da Trybe tem duração de 12 meses e é focado em desenvolvimento de software,
+                 banco de dados, análise de sistemas e programação.</p>
+            </div>
+            <div className="py-10">
+                <h3 className="text-2xl font-bold text-center animate__animated animate__fadeIn">Sistema de Informação - FURG</h3>
+                <p className="mt-4 text-center animate__animated animate__fadeIn">2017 - 2020 (incompleto), o curso de Sistema de informação tem duração de 4 anos, e é focado em desenvolvimento de sistemas e gerenciamento.</p>
+            </div>
+        </section>
+    );
+};
